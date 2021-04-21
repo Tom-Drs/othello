fc7dac6b7d786f4ac263427d3ddf6c25ce97675b
TomyDevD <tomdamerose@gmail.com>
2021-04-21 11:51:38 +0200

    modification of texts in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     player = othello.Player(1)
     grid = othello.Grid()
     while True:
-        print("C'est à vous de jouer")
+        print("C'est à vous de jouer (vous êtes noir).")
         print("--------------------------------------")
         grid.print_grid()
         if grid.best_move(1) is not None:
@@ -14,21 +14,21 @@ def main():
             while grid.player_put_pawn((int(pos[0]), int(pos[1])), 1) is None:
                 print("Position rentrée incorrecte.")
                 pos = input("Entrez une position pour poser un pion.")
-            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}")
+            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}.")
         else:
             print("La partie est terminé !")
-            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}")
+            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}.")
             return
         print("--------------------------------------")
         print("Le bot est en train de jouer...")
         if grid.best_move(2) is not None:
             move = grid.best_move(2)
             grid.player_put_pawn((move[1][0], move[1][1]), 2)
-            print(f"Le bot à posé son pion en {move[1][0], [move[1][1]]}")
-            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}")
+            print(f"Le bot a posé son pion en {move[1][0], move[1][1]}.")
+            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}.")
         else:
             print("La partie est terminé !")
-            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}")
+            print(f"Vos points : {player.calculate_points(grid)} \nPoints du bot : {bot.calculate_points(grid)}.")
             return
 
 if __name__ == "__main__":
